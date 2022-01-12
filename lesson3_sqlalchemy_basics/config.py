18c1e6130578524b331d7fc69e5acee5fcf3e28b
nikhilpandey-dev <nikhilpandey4@gmail.com>
2022-01-12 11:10:00 +0530

    Added database.ini file for reading database credentials.

---
+++ lesson3_sqlalchemy_basics/config.py
@@ -0,0 +1,15 @@
+from configparser import ConfigParser
+from typing import List, Dict, Tuple
+def config(filename="database.ini", section="postgresql"):
+    # Create Parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+    db: Dict[str, str] = dict()
+    if parser.has_section(section):
+        params: List[Tuple[str, str]] = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} is not found in {1} file'.format(section, filename))
+    return db
